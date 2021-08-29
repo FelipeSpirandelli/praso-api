c66f43a7f6bacf9deab601d998417ca2ce141664
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2021-08-29 10:23:24 -0300

    config postgres

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { indexPage, productsPage } from '../controllers';
+import { indexPage, productsPage, messagesPage } from '../controllers';
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 
 indexRouter.get('/products', productsPage);
+
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
